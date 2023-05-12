0b26ee8602593cf8f43d1db6705e4cd786b916db
zidsly <m.zidan.satrio@gmail.com>
2023-05-12 21:23:13 +0700

    first commit

---
+++ Dompet Pintar/src/base/INTO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package base;
+
+/**
+ *
+ * @author Zids
+ */
+class INTO {
+              /*  
+    
+    
+        try{
+            if(txtAddBalance.getText().equals("")){
+                        JOptionPane.showMessageDialog(this, "saldo yang ditambahkan 0", "Pesan", JOptionPane.ERROR_MESSAGE);
+                hapuslayar();
+                }else{
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    con=DriverManager.getConnection("jdbc:mysql://localhost/dbakun","root","");
+                     st = con.createStatement();
+                     String simpan = "INSERT INTO tblbalance(balance) VALUES('"+txtAddBalance.getText()+ "')";
+                        st = con.createStatement();
+                        st.executeUpdate(simpan);
+                        JOptionPane.showMessageDialog(this, "saldo berhasil ditambahkan, transaksasi berhasil");
+                        this.setVisible(false);
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(this,"This Account is lready exist / Duplicate Account ", "Pesan",
+                JOptionPane.WARNING_MESSAGE);
+            System.out.println(e);
+                    hapuslayar();
+                            */
+}
---
+++ Dompet Pintar/src/base/st.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package base;
+
+/**
+ *
+ * @author user
+ */
+class st {
+
+    static void executeQuery(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
